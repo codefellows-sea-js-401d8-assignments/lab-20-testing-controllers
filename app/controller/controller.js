0d76f4007ca5798db459064cbaa2d3fac936107c
jp <gaines.johnp@gmail.com>
2016-08-17 12:54:21 -0700

    latest work...needs work still, incomplete

---
+++ app/controller/controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const angular = require('angular');
+const demoApp = angular.module('demoApp');
+
+demoApp.controller = ('PlayerController', ['$scope', function($scope) {
+  $scope.playerCtrl = {};
+  $scope.playerCtrl.isDone = false;
+  $scope.playerCtrl.classes = ['stealth', 'friend', 'sneaker', 'jammer'];
+
+  $scope.playerCtrl.signUp = function() {
+    $scope.playerCtrl.isDone = true;
+  } ;
+
+}]);
