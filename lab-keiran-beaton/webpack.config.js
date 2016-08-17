52709763ada14cdf6d08352dabddbc28a03c9fd6
keiran beaton <keiran beaton>
2016-08-16 23:25:51 -0700

    Revert "need to add tests"
    This reverts commit cb2037adb865de272e3e9c864e5fd98d72bbe1ed.

--- lab-keiran-beaton/webpack.config.js
+++ lab-keiran-beaton/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     loaders: [
       {
         test: /\.scss$/,
-        loader: ExtractText.extract('style', 'css!sass!'),
+        loader: ExtractText.extract['style', 'css!sass!']
       },
       {
         test:/\.js$/,
