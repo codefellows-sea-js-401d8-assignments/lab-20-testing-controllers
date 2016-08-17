5a98c8b84d236b9717c170ff1a72b4d8b35ed7e9
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-16 20:38:28 -0700

    mvp + bonus

--- lab-james/karma.conf.js
+++ lab-james/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'app/entry.js',
-      'test/*-test.js',
+      'test/*_test.js',
       'node_modules/angular-mocks/angular-mocks.js'
     ],
 
@@ -30,12 +30,12 @@ module.exports = function(config) {
 
     // webpack config file to use
     webpack: webpackConfig,
-    
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'app/entry.js': ['webpack'],
-      'test/*-test.js': ['babel']
+      'test/*_test.js': ['babel']
     },
 
 
