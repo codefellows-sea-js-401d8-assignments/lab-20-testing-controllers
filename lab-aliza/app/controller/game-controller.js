c122b755eb331992e1ed8cab4880a0eb15d52350
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-17 08:47:01 -0700

    finished game-controller

---
+++ lab-aliza/app/controller/game-controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const angular = require('angular');
+const gameApp = angular.module('gameApp');
+
+gameApp.controller('GameController', ['$scope', function($scope){
+  $scope.gameCtrl = {};
+  $scope.gameCtrl.player = {};
+  $scope.gameCtrl.monster = {};
+  $scope.gameCtrl.player.isDone = false;
+  $scope.gameCtrl.player.classes = ['wizard', 'nerd', 'hipster', 'puppy'];
+  $scope.gameCtrl.monster.classes = ['crow', 'slug', 'dragon', 'muppet'];
+  $scope.gameCtrl.monster.isDone = false;
+  $scope.gameCtrl.player.isDone = false;
+
+  $scope.gameCtrl.createPlayer = function(player) {
+    $scope.gameCtrl.player = player;
+    $scope.gameCtrl.player.isDone = true;
+  };
+
+  $scope.gameCtrl.createMonster = function(monster) {
+    $scope.gameCtrl.monster = monster;
+    $scope.gameCtrl.monster.isDone = true;
+  };
+}]);
