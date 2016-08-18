603a265c52112fa82f093f18b92ec865eea74b43
Tracey <radcliffe.tracey@gmail.com>
2016-08-17 21:55:22 -0700

    monster is working

--- lab-traceyr/app/controller/game-controller.js
+++ lab-traceyr/app/controller/game-controller.js
@@ -6,7 +6,7 @@ const lab20 = angular.module('lab-20');
 lab20.controller('GameController', ['$scope', function($scope){
   $scope.gameCtrl = {};
   $scope.gameCtrl.playerClasses = ['Elf', 'Hobbit', 'Wizard', 'Human', 'Dwarf', 'Ent'];
-  $scope.gameCtrl.monsters = ['Orc', 'Balrog', 'Dragon', 'Goblin', 'Ringwraith', 'Troll'];
+  $scope.gameCtrl.monsterTypes = ['Orc', 'Balrog', 'Dragon', 'Goblin', 'Ringwraith', 'Troll'];
   $scope.gameCtrl.playerDone = false;
   $scope.gameCtrl.monsterDone = false;
 
