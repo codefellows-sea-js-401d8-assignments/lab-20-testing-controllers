4f32b4d929e38603f6916c0d7e9ab636c93eb68a
Firefly4266 <johnathon@Icon-Base.(none)>
2016-08-21 17:10:06 -0700

    Added monster controller and more refactor.  Still functional.

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -3,14 +3,22 @@
 const angular = require('angular');
 const demoApp = angular.module('demoApp');
 
-demoApp.controller('PlayerController', ['$scope', function($scope) {
+demoApp.controller('GameController', ['$scope', function($scope) {
   $scope.playerCtrl = {};
   $scope.playerCtrl.isDone = false;
   $scope.playerCtrl.classes = ['stealth', 'friend', 'sneaker', 'jammer'];
 
   $scope.playerCtrl.signUp = function(player) {
+    //creates the player
     $scope.playerCtrl.player = player;
     $scope.playerCtrl.isDone = true;
   } ;
+  $scope.monsterCtrl = {};
+  $scope.monsterCtrl.isDone = false;
+  $scope.monsterCtrl.classes = ['Villian', 'Creeper', 'Clinton', 'Trump'];
 
+  $scope.monsterCtrl.signUp = function(monster) {
+    $scope.monsterCtrl.monster = monster;
+    $scope.monsterCtrl.isDone = true;
+  } ;
 }]);
